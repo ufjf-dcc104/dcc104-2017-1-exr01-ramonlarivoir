42a0e762562a851f6e4b2c148145a7653e3e7264
Ramon Larivoir <rlarivoir@hotmail.com>
2017-03-22 20:45:30 -0300

    Adiciona controle e Sprite

---
+++ Sprite.js
@@ -0,0 +1,26 @@
+
+function Sprite() {
+  this.x = 0;
+  this.y = 0;
+  this.vx = 0;
+  this.vy = 0;
+  this.ax = 0;
+  this.ay = 0;
+  this.color = "blue";
+
+}
+
+Sprite.prototype.desenhar = function (ctx) {
+  ctx.fillStyle = this.color;
+  ctx.fillRect(this.x, this.y, 15, 15);
+  ctx.strokeStyle = "black";
+  ctx.strokeRect(this.x, this.y, 15, 15);
+};
+
+Sprite.prototype.mover = function (dt) {
+  this.vx = this.vx + this.ax*dt;
+  this.vy = this.vy + (this.ay+60)*dt;
+  this.x = this.x + this.vx*dt;
+  this.y = this.y + this.vy*dt;
+
+}
